17c7e716ec705b00385cb60361bfbd71d6a8ad74
gabe-russell <gabriel.russell@maine.edu>
2024-10-16 15:46:54 -0400

    adding table columns

--- chemicalinventory/src/App.js
+++ chemicalinventory/src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React, { useState } from "react";
 import { 
   TextField, 
@@ -15,7 +14,7 @@ import './App.css';
 
 const App = () => {
   const [inventory, setInventory] = useState([]);
-  const [newItem, setNewItem] = useState({ name: "", quantity: "", location: "" });
+  const [newItem, setNewItem] = useState({ name: "", quantity: "", location: "" , casnumber: "", manufacturer: "", weight: "", numcontainer: "", sds: ""});
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -25,7 +24,7 @@ const App = () => {
   const addItem = () => {
     if (newItem.name && newItem.quantity && newItem.location) {
       setInventory([...inventory, newItem]);
-      setNewItem({ name: "", quantity: "", location: "" });
+      setNewItem({ name: "", quantity: "", location: "" , casnumber: "", manufacturer: "", weight: "", numcontainer: "", sds: ""});
     }
   };
 
@@ -49,9 +48,44 @@ const App = () => {
           className="input"
         />
         <TextField
+          label="CASNumber"
+          name="casnumber"
+          value={newItem.casnumber}
+          onChange={handleInputChange}
+          className="input"
+        />
+        <TextField
           label="Location"
           name="location"
           value={newItem.location}
+          onChange={handleInputChange}
+          className="input"
+        />
+        <TextField
+          label="Manufacturer"
+          name="manufacturer"
+          value={newItem.manufacturer}
+          onChange={handleInputChange}
+          className="input"
+        />
+        <TextField
+          label="Weight"
+          name="weight"
+          value={newItem.weight}
+          onChange={handleInputChange}
+          className="input"
+        />
+        <TextField
+          label="Number of Containers"
+          name="numcontainer"
+          value={newItem.numcontainer}
+          onChange={handleInputChange}
+          className="input"
+        />
+        <TextField
+          label="SDS Website"
+          name="sds"
+          value={newItem.sds}
           onChange={handleInputChange}
           className="input"
         />
@@ -71,7 +105,12 @@ const App = () => {
             <TableRow>
               <TableCell><strong>Item Name</strong></TableCell>
               <TableCell><strong>Quantity</strong></TableCell>
+              <TableCell><strong>CAS Number</strong></TableCell>
+              <TableCell><strong>Manufacturer</strong></TableCell>
+              <TableCell><strong>Weight</strong></TableCell>
+              <TableCell><strong>Number of Containers</strong></TableCell>
               <TableCell><strong>Location</strong></TableCell>
+              <TableCell><strong>SDS</strong></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -79,13 +118,18 @@ const App = () => {
               <TableRow key={index}>
                 <TableCell>{item.name}</TableCell>
                 <TableCell>{item.quantity}</TableCell>
+                <TableCell>{item.casnumber}</TableCell>
+                <TableCell>{item.manufacturer}</TableCell>
+                <TableCell>{item.weight}</TableCell>
+                <TableCell>{item.numcontainer}</TableCell>
                 <TableCell>{item.location}</TableCell>
+                <TableCell>{item.sds}</TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
-      <p>Created by Code 007</p>
+      <p><center>Created by Code 007</center></p>
     </div>
   );
 };
