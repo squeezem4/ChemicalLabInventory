a3f1c40eda63bcde2c19b36586b27f383041e69f
gabe-russell <gabriel.russell@maine.edu>
2024-10-30 13:04:12 -0400

    added authorization and login component

--- chemicalinventory/src/firebaseConfig.js
+++ chemicalinventory/src/firebaseConfig.js
@@ -17,5 +17,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
