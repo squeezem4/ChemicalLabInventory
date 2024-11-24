95a958c834caecb3c55557eab6efa4e7208441d5
Ryan Nodarse <ryan.nodarse@maine.edu>
2024-11-24 12:35:26 -0500

    jest testing practice

--- chemicalinventory/src/App.test.js
+++ chemicalinventory/src/App.test.js
@@ -1,15 +1,9 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
+// import React from "react";
 import App from './App';
 
-test("initially load login page", () => {
-  expect(sum(1, 2)).toBe(3)
-})
-// describe(App, () => {
-//   it()
-// })
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test("example test", () => {
+  expect((1 + 2)).toBe(3);
+})
+// describe
--- chemicalinventory/src/components/chemicalinventory.test.js
+++ chemicalinventory/src/components/chemicalinventory.test.js
@@ -1,5 +1,64 @@
+import { render, screen } from '@testing-library/react';
+import React, {useState} from 'react';
+import PropTypes, { object } from "prop-types";
 import ChemicalInventory from './chemicalinventory';
 
-test("initially load login page", () => {
-    expect(sum(1, 2)).toBe(3)
-  })
+const HelloWorld = () => <h1>HelloWorld</h1>
+test("it should render items", () => {
+  const items = ["item 1", "item 2", "item 3"];
+  render(<ItemList items = {items} />);
+  screen.debug();
+  screen.getByText(/item 1/i);
+})
+
+test("it should be able to handle an empty list", () => {
+  const items = [];
+  render(<ItemList items = {items} />);
+  screen.debug();
+  screen.getByText(/no items/i);
+})
+
+test("it should render if given no items", () => {
+  render(<ItemList/>);
+  screen.debug();
+  screen.getByText(/no items/i);
+})
+
+test("it should render an error", () => {
+  const error = new Error ("There was an error");
+  render(<ItemList error = {error}/>);
+  screen.debug();
+  screen.getByText(/no items/i);
+})
+
+test("it should show loading", () => {
+  const isLoading = true;
+  render(<ItemList isLoading = {isLoading}/>);
+  screen.debug();
+  screen.getByText(/Loading/i);
+})
+
+const ItemList = ({items = [], isLoading = false, error}) => {
+  console.log({items});
+  if(isLoading){
+    return <div>Loading</div>
+  }
+  // const[items, setItems] = useState(["item 1", "item 2", "item 3"]);
+  return (
+    <div>
+      <div>
+        {error && <div>{error.message}</div>}
+      </div>
+      <div>
+        {items.length === 0 && <div>no items</div>}
+      </div>
+      <ul>
+        {items.map((item, index) => <li key = {index}> {item} </li>)}
+      </ul>
+    </div>
+  )
+}
+
+ItemList.propTypes = {
+  items: PropTypes.arrayOf (PropTypes.string)
+}
