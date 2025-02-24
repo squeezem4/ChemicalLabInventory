333aba500e81869bd9d9db32e61493dac74c8248
Ryan Nodarse <ryan.nodarse@maine.edu>
2025-02-24 16:39:19 -0500

    Added relevance to a basic test

--- chemicalinventory/src/App.test.js
+++ chemicalinventory/src/App.test.js
@@ -1,9 +1,9 @@
-// import { render, screen } from '@testing-library/react';
-// import React from "react";
+import React from "react";
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-
-test("example test", () => {
-  expect((1 + 2)).toBe(3);
-})
-// describe
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
