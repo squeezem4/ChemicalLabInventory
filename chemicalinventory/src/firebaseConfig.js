58a42694267b348e50a9098684e9e94164837ac4
gabe-russell <gabriel.russell@maine.edu>
2025-05-02 16:13:10 -0400

    fixing api key error

--- chemicalinventory/src/firebaseConfig.js
+++ chemicalinventory/src/firebaseConfig.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 //will need to add professors firebase when ready for production
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGESENDERID,
-  appId: process.env.APPID,
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGESENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
 
 // Initialize Firebase
